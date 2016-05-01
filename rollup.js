e4ea84196dc0b6a7006bdbc0864ad90cb39c65e6
Rob Wormald <robwormald@gmail.com>
2016-05-01 14:15:35 -0700

    rollup

---
+++ rollup.js
@@ -0,0 +1,29 @@
+//rollup hack
+import {rollup} from 'rollup'
+import * as path from 'path'
+
+class RollupNG2 {
+    constructor(options){
+        this.options = options;
+    }
+    resolveId(id, from){
+        if(id.startsWith('angular2/')){
+            return `${__dirname}/vendor/angular2/${id.split('angular2/').pop()}.js`;
+        }
+        if(id.startsWith('rxjs/')){
+            return `${__dirname}/node_modules/rxjs-es/${id.split('rxjs/').pop()}.js`;
+        }
+    }
+}
+
+
+const rollupNG2 = (config) => new RollupNG2(config);
+
+
+export default {
+    entry: 'tmp/main-static.js',
+	format: 'iife',
+	dest: 'tmp/build.js',
+    plugins: [rollupNG2()],
+
+}
